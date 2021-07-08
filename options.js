b96b123cbca6a8933eac6a098371dc469f7f6954
AngelDOrtiz <79877594+AngelDOrtiz@users.noreply.github.com>
2021-07-08 16:53:32 -0700

    added color options for extension (#6)

--- options.js
+++ options.js
@@ -1,7 +1,7 @@
 let page = document.getElementById("buttonDiv");
-
 let selectedClassName = "current"
 const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"]
+
 
 
 function handleButtonClick(event) {
--- popup.js
+++ popup.js
@@ -1,5 +1,14 @@
 let changeColor = document.getElementById("changeColor");
 let changeFontSize = document.getElementById("changeFontSize");
+// import iro from '@jaames/iro';
+
+
+const colorPicker = new iro.ColorPicker("#picker")
+
+chrome.tabs.excecuteScript(tab.id, {
+    code:
+        "document.body.appendChild(document.createElement('script')).src = 'https://cdn.jsdelivr.net/npm/@jaames/iro@5';"
+})
 
 chrome.storage.sync.get("fontSize", ({ fontSize }) => {
     changeColor.style.fontSize = fontSize;
@@ -7,9 +16,9 @@ chrome.storage.sync.get("fontSize", ({ fontSize }) => {
 
 chrome.storage.sync.get("color", ({ color }) => {
     changeColor.style.backgroundColor = color;
-  });
+});
 
-changeColor.addEventListener("click", async() => {
+changeColor.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
@@ -18,7 +27,7 @@ changeColor.addEventListener("click", async() => {
     });
 });
 
-changeFontSize.addEventListener("click", async() => {
+changeFontSize.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
@@ -28,7 +37,7 @@ changeFontSize.addEventListener("click", async() => {
 
 function setPageBackgroundColor() {
     chrome.storage.sync.get("color", ({ color }) => {
-       document.body.style.backgroundColor = color;
+        document.body.style.backgroundColor = color;
     });
 }
 
