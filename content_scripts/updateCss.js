f79fbfc7e65a2bdd630a84fcb5e3ee266a20a053
Kalan Prudhomme <33067232+prudhomk@users.noreply.github.com>
2021-07-12 15:17:55 -0700

    Kalan branch (#12)
    * added font input field
    
    * working on color picker
    
    * implementing substitution function
    
    * extension works!!
    
    * removed window check

--- content_scripts/updateCss.js
+++ content_scripts/updateCss.js
@@ -4,11 +4,12 @@ function updateCSS(request, sender, sendResponse) {
     const html = document.querySelector('html');
     const body = document.querySelector('body');
 
+
     if (request.color) {
         html.style.backgroundColor = request.color;
         body.style.backgroundColor = request.color;
     } else if (request.reset) {
-        html.style.backgroundColor = "";
-        body.style.backgroundColor = "";
+        html.style.backgroundColor = '';
+        body.style.backgroundColor = '';
     }
 }
