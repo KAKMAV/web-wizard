3353bbfda6a6d8007246ee1b695f55c306f29212
Vijay Chakilam <vijaybchakilam@gmail.com>
2021-07-13 19:49:05 -0400

    text content change (partial) (#20)
    * added font input field
    
    * working on color picker
    
    * implementing substitution function
    
    * extension works!!
    
    * removed window check
    
    * change font family
    
    * added alter textcontent functionality
    
    * handle browser tab sendMessage promise correctly
    
    * working on textcontent functionality
    
    * change text content
    
    Co-authored-by: Kalan Prudhomme <cleaver9@gmail.com>

--- content_scripts/updateCss.js
+++ content_scripts/updateCss.js
@@ -1,5 +1,22 @@
 browser.runtime.onMessage.addListener(updateCSS);
 
+function replaceText(wordMap, node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    if (node.parentNode && node.parentNode.nodeName === 'TEXTAREA') {
+      return;
+    }
+    let content = node.textContent;
+    for (let word in wordMap) {
+      content = content.replace(word, wordMap[word]);
+    }
+    node.textContent = content;
+  } else {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      replaceText(wordMap, node.childNodes[i]);
+    }
+  }
+}
+
 function updateCSS(request, sender, sendResponse) {
   const html = document.querySelector('html');
   const body = document.querySelector('body');
@@ -10,12 +27,13 @@ function updateCSS(request, sender, sendResponse) {
   } else if (request.fontSize) {
     html.style.fontSize = request.fontSize;
     body.style.fontSize = request.fontSize;
+    span.style.fontSize = request.fontSize;
   } else if (request.fontFamily) {
     html.style.fontFamily = request.fontFamily;
     body.style.fontFamily = request.fontFamily;
   } else if (request.textContent) {
-    html.textContent = request.textContent;
-    body.textContent = request.textContent;
+    const wordMap = { about: request.textContent };
+    replaceText(wordMap, document.body);
   } else if (request.reset) {
     html.style.backgroundColor = '';
     body.style.backgroundColor = '';
